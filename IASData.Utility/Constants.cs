82c72ad6120388c3fb97b35b1a7d347e555b2e19
mabbaszadegan <mhmabbaszadegan@gmail.com>
2018-08-30 17:12:39 +0430

    افزودن کلاس جهت نگهداری مقادیر ثابت در برنامه

---
+++ IASData.Utility/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IASData.Utility
+{
+    class Constants
+    {
+        const string AttachmentServiceUrl = "http://82.99.218.100:8080/Services/AttachmentShow.ashx";
+    }
+}
