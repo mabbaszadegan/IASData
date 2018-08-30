c05520dabb9cdbcf71dc315aeb3fae455237283d
mabbaszadegan <mhmabbaszadegan@gmail.com>
2018-08-30 18:11:07 +0430

    no message

--- Kuchegardan/obj/Debug/Package/PackageTmp/MyScripts/Controllers/Default/DefaultController.js
+++
@@ -1,9 +0,0 @@
-﻿defaultApp.controller("defaultController",
-    function ($scope, $location, DefaultService, ShareData) {
-        if (sessionStorage["userId"] === undefined ||
-            sessionStorage["userId"] == null ||
-            sessionStorage["userId"] === "0" ||
-            sessionStorage["userId"] === "null")
-            $location.path("Login");
-
-    });
