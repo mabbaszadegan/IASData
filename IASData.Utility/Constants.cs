a0fca14eecc4c79cae374eb32f0656e5254ca089
mabbaszadegan <mhmabbaszadegan@gmail.com>
2018-09-14 20:10:32 +0430

    خواندن آدرس منابع استاتیک از وب کانفیگ

--- IASData.Utility/Constants.cs
+++ IASData.Utility/Constants.cs
@@ -4,4 +4,8 @@
     {
         public const string AttachmentServiceUrl = "http://82.99.218.100:8080/Services/";
     }
+    public static class ConfigReader {
+        public static string ReferenceSeverUrl = System.Web.Configuration.WebConfigurationManager.AppSettings["ReferenceSeverUrl"];
+   
+    }
 }
